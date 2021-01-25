29104c14b0766fa2a826020999630a7cffb656e1
Rupam <hrupam55@gmail.com>
2021-01-25 14:16:05 +0530

    added decreaseQuantity() with setState callback

--- src/CartItem.js
+++ src/CartItem.js
@@ -12,16 +12,28 @@ class CartItem extends React.Component {
     // this.increaseQuantity = this.increaseQuantity.bind(this);
   }
   increaseQuantity = () => {
-    // setState method 1
+    // setState method 1 - when we dont need previous state
     // this.setState({ qty: this.state.qty + 1 });
     // console.log(this.state);
 
-    // setState method 2
+    // setState method 2 - when we require previous state
     this.setState((prevState) => {
       return {
         qty: prevState.qty + 1,
       };
     });
+  };
+  decreaseQuantity = () => {
+    this.setState(
+      (prevState) => {
+        if (prevState.qty === 1) return {};
+        else
+          return {
+            qty: prevState.qty - 1,
+          };
+      },
+      () => console.log(this.state) //callback executed when state updation is finished
+    );
   };
   render() {
     const { price, title, qty } = this.state;
@@ -46,6 +58,7 @@ class CartItem extends React.Component {
               alt="decrease"
               className="action-icons"
               src="https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"
+              onClick={this.decreaseQuantity}
             />
             <img
               alt="delete"
