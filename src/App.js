92f453a91b7bcbd1b5dca72b207c591c02dd7d0e
Rupam <hrupam55@gmail.com>
2021-01-27 21:13:05 +0530

    made products a separate file

--- src/App.js
+++ src/App.js
@@ -2,45 +2,12 @@ import React from "react";
 import Cart from "./Cart";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import products from "./products";
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: [
-        {
-          price: 5999,
-          title: "Phone",
-          qty: 0,
-          img: "https://static.toiimg.com/photo/70072353.cms",
-          img_alt: "phone",
-          id: 1,
-        },
-        {
-          price: 28599,
-          title: "Camera",
-          qty: 0,
-          img: "https://cdn.mos.cms.futurecdn.net/mVEhQU5grHExywfQsDoAYj.jpg",
-          img_alt: "camera",
-          id: 2,
-        },
-        {
-          price: 36000,
-          title: "Laptop",
-          qty: 0,
-          img: "https://cdn.mos.cms.futurecdn.net/6t8Zh249QiFmVnkQdCCtHK.jpg",
-          img_alt: "laptop",
-          id: 3,
-        },
-        {
-          price: 549,
-          title: "Headphone",
-          qty: 0,
-          img:
-            "https://i.gadgets360cdn.com/products/headphones-and-headsets/large/boat-rockerz-400-wireless-headphone-832X558-1598367902.jpg",
-          img_alt: "headphone",
-          id: 4,
-        },
-      ],
+      products: products,
     };
   }
   handleIncreaseQuantity = (product) => {
---
+++ src/products.js
@@ -0,0 +1,37 @@
+let products = [
+  {
+    price: 5999,
+    title: "Phone",
+    qty: 0,
+    img: "https://static.toiimg.com/photo/70072353.cms",
+    img_alt: "phone",
+    id: 1,
+  },
+  {
+    price: 28599,
+    title: "Camera",
+    qty: 0,
+    img: "https://cdn.mos.cms.futurecdn.net/mVEhQU5grHExywfQsDoAYj.jpg",
+    img_alt: "camera",
+    id: 2,
+  },
+  {
+    price: 36000,
+    title: "Laptop",
+    qty: 0,
+    img: "https://cdn.mos.cms.futurecdn.net/6t8Zh249QiFmVnkQdCCtHK.jpg",
+    img_alt: "laptop",
+    id: 3,
+  },
+  {
+    price: 549,
+    title: "Headphone",
+    qty: 0,
+    img:
+      "https://i.gadgets360cdn.com/products/headphones-and-headsets/large/boat-rockerz-400-wireless-headphone-832X558-1598367902.jpg",
+    img_alt: "headphone",
+    id: 4,
+  },
+];
+
+export default products;
